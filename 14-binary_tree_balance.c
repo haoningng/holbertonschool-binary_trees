abd94d3eb0d34aa4686b326fdbcd95174d4b60f5
haoningng <nick.nghn@gmail.com>
2024-01-12 02:33:46 -0800

    fix: delete checking for leaf nodes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,12 +11,16 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int balance;
+	int left_height;
+	int right_height;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right != NULL)
-		return (-1);
-	balance = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+	
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	balance = left_height - right_height;
 	return (balance);
 }
 
