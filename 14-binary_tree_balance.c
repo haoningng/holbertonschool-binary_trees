cf78774c3d81965e7da2e3da69aaf4b7e71e73da
haoningng <nick.nghn@gmail.com>
2024-01-14 15:21:11 -0800

    fix: edit the function to calculate height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,7 +16,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
@@ -38,7 +38,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
